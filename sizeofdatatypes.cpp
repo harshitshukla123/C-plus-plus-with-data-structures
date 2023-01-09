157024481da3fa2c7d1455996d4c30134d09c8fa
harshitshukla123 <104128682+harshitshukla123@users.noreply.github.com>
2023-01-09 17:16:11 +0530

    Add files via upload

---
+++ sizeofdatatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    char b;
+    float c;
+    bool d;
+    cout<<sizeof(a)<<"\n";
+    cout<<sizeof(b)<<"\n";
+    cout<<sizeof(c)<<"\n";
+    cout<<sizeof(d)<<"\n";
+    return 0;
+}
